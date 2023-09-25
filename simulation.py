9268a0610401ca3f5136c249e5ad2a7e5721b874
Pedro <pbbdasilva@gmail.com>
2023-09-25 11:26:27 -0300

    small fixes to run in samples

--- simulation.py
+++ simulation.py
@@ -1,5 +1,9 @@
 import numpy as np
 from statistics import mean
+import argparse
+import copy
+import concurrent.futures
+
 # Define the state space
 states = ["late", "ok",
           "heavy_jam", "light_jam", "no_jam_late", "no_jam_ok",
@@ -53,7 +57,6 @@ blue_flux = {
 
 p_acc = mean([0.05, 0.07, 0.005, 0.005, 0.0075, 0.01]) # chance of accident
 p_start_late = 0.3 # 30% starting late, 70% starting on time
-fluxes = [blue_flux, yellow_flux, red_flux]
 def run_sample(fluxes, rain):
     # Initial state
     current_state = np.random.choice(states, p=[p_start_late, 1-p_start_late, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) 
@@ -63,38 +66,54 @@ def run_sample(fluxes, rain):
     for i in range(num_steps*len(fluxes)):
         current_state = np.random.choice(states, p=get_transition_matrix(fluxes[int(i/4)], rain)[states.index(current_state)])
         simulated_states.append(current_state)
-    print("Simulated States:", simulated_states)
     return simulated_states
 
 hurry_factor = 0.8 #how fast student go if he thinks he is late
 rain_factor = 0.9 #downgrade in speed due rain (internet fonts said it should be 80% of the speed, but in the target route there is no space to many free road where it should apply hard)
-def get_final_time(avr_times, simulated_states, rain):
-    if len(avr_times) != ((len(simulated_states) - 1)/4):
-        return "error: number of time intervals incompatible with these simulated_states"
-    final_time = 0
+def get_final_time(avg_times, simulated_states, rain):
+    final_time = 0.0
     if simulated_states[0] == "late":
         final_time += 5
-        avr_times[0] *= hurry_factor
+        avg_times[0] *= hurry_factor
     for happening in simulated_states[1:]:
         match happening:
             case "heavy_jam":
-                avr_times[0] *= 1.1
+                avg_times[0] *= 1.1
             case "no_jam_late" | "no_jam_ok":
-                avr_times[0] *= 0.9
+                avg_times[0] *= 0.9
             case "acc_jam":
-                avr_times[0] *= 1.32
+                avg_times[0] *= 1.32
             case "jam_closed" | "no_jam_late_closed" | "no_jam_ok_closed":
-                avr_times[0] += 0.125 # I took a sample of 15s delay in semaphore, where
+                avg_times[0] += 0.125 # I took a sample of 15s delay in semaphore, where
             case "ok":
-                final_time += avr_times[0]
-                avr_times.pop(0)
+                final_time += avg_times[0]
+                avg_times.pop(0)
             case "late":
-                final_time += avr_times[0]
-                avr_times.pop(0)
-                if len(avr_times) > 0: avr_times[0]*hurry_factor
+                final_time += avg_times[0]
+                avg_times.pop(0)
+                if len(avg_times) > 0: avg_times[0] *= hurry_factor
             case _:
                 continue
     return final_time if not rain else final_time/0.9
-rain = True
-simulated_states = run_sample(fluxes, rain)
-print(get_final_time([4, 5, 6], simulated_states, rain))
+
+
+def simulate_single_run(fluxes, flux_times, rain):
+    return get_final_time(copy.deepcopy(flux_times), run_sample(fluxes, rain), rain)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simulate time for a given ')
+    parser.add_argument('--rain', type=bool, help='Add raining condition to simulation')
+    parser.add_argument('--runs', type=int, help='Number of runs')
+    args = parser.parse_args()
+    rain = args.rain
+    n = args.runs
+
+    fluxes = [blue_flux, blue_flux, yellow_flux, blue_flux, red_flux, yellow_flux, yellow_flux, yellow_flux, blue_flux]
+    flux_times = [0.5, 1.0, 0.5, 2.0, 3.0, 1.0, 1.0, 2.0, 1.0]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:  # You can also use ProcessPoolExecutor for processes
+        results = list(executor.map(simulate_single_run, [fluxes] * n, [flux_times] * n, [rain] * n))
+
+    with open('results2.txt', 'a') as f:
+        for row in results:
+            f.write(str(row) + '\n')
